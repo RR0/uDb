5e38f43c4e09f2fcd58e3caaffc27e76095ba10a
javarome <javarome@gmail.com>
2017-04-03 14:24:20 +0200

    Fixed time display

--- time.ts
+++ time.ts
@@ -12,30 +12,30 @@ export class Time {
       case 3:
         accurateValue += value;
     }
-    let num = parseInt(accurateValue);
-    return isNaN(num) ? accurateValue : num;
+    let num = parseInt(accurateValue, 10);
+    return num.toString() != accurateValue ? accurateValue : num;
   }
 
   static getDay(record, padding = false) {
-    const dayAccuracy = (record.ymdt >> 2) & 3;
+    const dayAccuracy = (record.ymdt >> 2) & 0b11;
     let day = padding ? (record.day > 31 ? '--' : (record.day < 10 ? '0' : '') + record.day) : record.day;
     return Time.accuracy(day, dayAccuracy, padding);
   }
 
   static getMonth = function (record, padding = false) {
-    const monthAccuracy = (record.ymdt >> 4) & 3;
+    const monthAccuracy = (record.ymdt >> 4) & 0b11;
     let month = padding ? (record.month < 10 ? '0' : '') + record.month : record.month;
     return Time.accuracy(month, monthAccuracy, padding);
   };
 
   static getYear(record) {
-    const yearAccuracy = (record.ymdt >> 6) & 3;
+    const yearAccuracy = (record.ymdt >> 6) & 0b11;
     return Time.accuracy(record.year, yearAccuracy);
   }
 
   static getTime(record): string | number {
-    let time = record.hour;
-    const timeAccuracy = record.ymdt & 3;
+    let time = record.time;
+    const timeAccuracy = record.ymdt & 0b11;
     let hours = Math.floor(time / 6);
     let minutes = (time % 6) * 10;
     return Time.accuracy((hours < 10 ? '0' : '') + hours + ':' + (minutes < 10 ? '0' : '') + minutes, timeAccuracy);
